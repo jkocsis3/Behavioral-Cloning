445c099877f60e63aa93d14ff09bc50703373be3
joe kocsis <joe.kocsis3@gmail.com>
2017-02-11 15:14:35 -0500

    updated Model.py with image preprocessing.

--- drive.py
+++ drive.py
@@ -19,6 +19,8 @@ from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_a
 import tensorflow as tf
 tf.python.control_flow_ops = tf
 
+import cv2
+
 
 sio = socketio.Server()
 app = Flask(__name__)
@@ -36,7 +38,14 @@ def telemetry(sid, data):
     # The current image from the center camera of the car
     imgString = data["image"]
     image = Image.open(BytesIO(base64.b64decode(imgString)))
+
+    # need to resize the images, adding this in from my file
+    image = np.array(image)
+    image = cv2.resize(image, (160, 320))
+    #back to original code
+
     image_array = np.asarray(image)
+
     transformed_image_array = image_array[None, :, :, :]
     # This model currently assumes that the features of the model are just the images. Feel free to change this.
     steering_angle = float(model.predict(transformed_image_array, batch_size=1))
--- model.py
+++ model.py
@@ -10,16 +10,18 @@ from PIL import Image
 import numpy as np
 from keras.models import Sequential
 from keras.layers.core import Dense, Activation, Flatten, Dropout
-from keras.layers import Convolution2D, Lambda
+from keras.layers import Convolution2D, Lambda, Cropping2D
 from keras.layers.pooling import MaxPooling2D
+from sklearn.utils import shuffle
 tf.python.control_flow_ops = tf
 import matplotlib.pyplot as plt
 # get_ipython().magic('matplotlib inline')
 
 
-cols = 160
-rows = 80
-
+width = 320
+height = 160
+newWidth = height
+newHeight = width
 
 # Open the data from the files
 def LoadImages(log, index, cols, rows):
@@ -28,7 +30,10 @@ def LoadImages(log, index, cols, rows):
     # print(log[index].split('/')[1])
     img = Image.open("./IMG/%s" % log[index].split('/')[1])
     img = np.array(img)
-    return cv2.resize(img, (cols, rows))
+    #img = img[50:160, 0:320]
+    # return img
+    # Resizing the picture tends to make the model train better.
+    return cv2.resize(img, (newWidth, newHeight))
 
 
 X = []
@@ -36,7 +41,7 @@ y = []
 steeringAdjust = 0.3
 
 # open log file
-print("Load .csv file")
+# print("Load .csv file")
 logs = []
 with open('driving_log.csv') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
@@ -44,80 +49,86 @@ with open('driving_log.csv') as csvfile:
         logs.append(row)
 
 for row in logs:
-    X.append(LoadImages(row, 0, cols, rows))
+    X.append(LoadImages(row, 0, width, height))
     y.append(float(row[3]))
-    X.append(LoadImages(row, 1, cols, rows))
+    X.append(LoadImages(row, 1, width, height))
     y.append(float(row[3]) + steeringAdjust)
-    X.append(LoadImages(row, 2, cols, rows))
-    y.append(float(row[3]) + steeringAdjust)
+    X.append(LoadImages(row, 2, width, height))
+    y.append(float(row[3]) - steeringAdjust)
 
-X = np.array(X)
-y = np.array(y)
+xTrain = np.array(X)
+yTrain = np.array(y)
 
 
 print("Images and labels loaded")
-
-
-# preprocessing
-# Load the data into training and test sets
-xTrain = X[:-2000]
-yTrain = y[:-2000]
-xTest = X[-2000]
-yTest = y[-2000]
-print("Train totals", xTrain.shape)
-print("Test totals", xTest.shape)
-
-
-
-print (xTrain[1].shape)
-
-image=xTrain[1]#.squeeze()
-plt.figure(figsize=(10,10))
-plt.imshow(image)
-print(yTrain[1])
-
-
-# One-Hot encode the labels
-label_binarizer = LabelBinarizer()
-y_one_hot = label_binarizer.fit_transform(yTrain)
-
-
-def normalize_grayscale(image_data):
-    a = -0.5
-    b = 0.5
-    grayscale_min = 0
-    grayscale_max = 255
-    return a + (((image_data - grayscale_min) * (b - a)) / (grayscale_max - grayscale_min))
-
-
-X_normalized = normalize_grayscale(xTrain)
-print("Done")
-
 """
-# Original Model, was not learning, so i tried to implement the NVidia model below.
-model = Sequential()
-model.add(Convolution2D(64, 3, 3, input_shape=(rows, cols, 3), dim_ordering='tf'))
-model.add(MaxPooling2D((2, 2), dim_ordering='tf'))
-model.add(Dropout(0.5))
-model.add(Activation('relu'))
-
-#model.add(Convolution2D())
-
-model.add(Flatten())
-
-model.add(Dense(128))
-model.add(Activation('relu'))
-
-model.add(Dense(1))
-model.add(Activation('softmax'))
-model.summary() # print model summary
+X, y = shuffle(X, y)
+xTrain = X
+yTrain = y
+# print (xTrain[1].shape)
 """
+#image=xTrain[1]#.squeeze()
+#plt.figure(figsize=(10,10))
+#plt.imshow(image)
+# print(yTrain[1])
+# image processing.  Flipping images, adding shadows,
+
+
+def ProcessImage(image, steerAngle):
+    # generate a random number,
+    num = np.random.randint(3)
+    if steerAngle != 0:
+        if num == 0:
+        # only want to flip the image if it is on a corner
+            if steerAngle != 0:
+                newImage, newSteering = FlipImage(image, steerAngle)
+                return newImage, newSteering
+    # This will randomly brighten or darken the image.
+    elif num == 1:
+        newImage = GammaAdjust(image)
+        return newImage, steerAngle
+    # default,
+    return image, steerAngle
+
+
+def FlipImage(image, steerAngle):
+    flippedImage = cv2.flip(image, 1)
+    flippedSteering = -steerAngle
+    return flippedImage, flippedSteering
+
+# brighen or darken the image
+# http://www.pyimagesearch.com/2015/10/05/opencv-gamma-correction/
+def GammaAdjust(image):
+    # build a lookup table mapping the pixel values [0, 255] to
+    # their adjusted gamma values
+    gamma = 0.2
+    invGamma = 1.0 / gamma
+    table = np.array([((i / 255.0) ** invGamma) * 255
+                      for i in np.arange(0, 256)]).astype("uint8")
+    # apply gamma correction using the lookup table
+    return cv2.LUT(image, table)
+
+index = 0
+xProcessed = []
+yProcessed = []
+for image in X:
+    newImage, newSteerAngle = ProcessImage(image, yTrain[index])
+    index += 1
+    xProcessed.append(newImage)
+    yProcessed.append(newSteerAngle)
+
+xProcessed = np.array(xProcessed)
+yProcessed = np.array(yProcessed)
+
+
+
 #NVIDIA Model
 #https://arxiv.org/pdf/1604.07316v1.pdf
 
 model = Sequential()
-
-model.add(Convolution2D(24,5,5, input_shape=(rows,cols,3), dim_ordering='tf'))
+model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(newHeight, newWidth,   3), dim_ordering='tf'))
+model.add(Lambda(lambda x: (x / 255) - 0.5 ))
+model.add(Convolution2D(24,5,5,  dim_ordering='tf', ), ) # input_shape=(newHeight, newWidth, 3),
 model.add(MaxPooling2D((2, 2), dim_ordering='tf'))
 model.add(Dropout(0.5))
 model.add(Activation('relu'))
@@ -145,20 +156,23 @@ model.add(Activation('relu'))
 model.add(Dense(50))
 model.add(Activation('relu'))
 
-
 model.add(Dense(10))
 model.add(Activation('relu'))
 
 model.add(Dense(1))
-#model.add(Activation('softmax'))
-
-
+# model.add(Activation('softmax'))
 
 model.summary()
 
+model.compile('adam', 'mean_squared_error', ['mean_absolute_error'])
+# print("begin training")
+history = model.fit(xProcessed, yProcessed, batch_size=128, nb_epoch=20, validation_split=0.2)
 
+#model.save("model.h5")
 
-model.compile('adam', 'mean_squared_error', ['accuracy'])
-print("begin training")
-history = model.fit(xTrain, yTrain, nb_epoch=5, validation_split=0.2)
+model_json = model.to_json()
+with open("model.json", "w") as json_file:
+    json_file.write(model_json)
+model.save_weights("model.h5")
 
+print("Model Saved")
